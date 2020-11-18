1ca2974b40c0a7468cf31671262e3de388a35e84
Sergei Pershin <sergei.pershin@live.com>
2020-11-18 11:27:25 +0200

    Homework for 3rd lesson Part 1 "Archive Deposits"

---
+++ src/test/java/AlfaBankTests.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AlfaBankTests {
+
+    @Test
+
+    void assertThatFiveDepositArchivePresentOnPage(){
+        open("http://alfabank.ru");
+        $("body").shouldHave(text("Частным лицам"));
+        $(byText("Вклады")).shouldBe(visible).click();
+        $("#filter").shouldHave(text("Накопительные продукты"));
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $(byText("Архивные счета и депозиты")).shouldBe(visible).click();
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $("#filter").$$("[data-widget-name=CatalogCard]").shouldHaveSize(5);
+    }
+}
+
